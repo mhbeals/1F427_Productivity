82b25913e3b08bee528f83365efff1a87caa4b52
Matt Williams <matt@milliams.com>
2018-03-28 10:41:36 +0100

    Make the Task.state field be an enum

--- core/models.py
+++ core/models.py
@@ -1,4 +1,15 @@
+import enum
+
 from django.db import models
+
+
+class ChoicesEnum(enum.IntEnum):
+    @classmethod
+    def choices(cls):
+        return tuple(((
+            en.value,
+            en.name.replace('_', ' ').title()
+        ) for en in cls))
 
 
 class Emoji(models.Model):
@@ -15,9 +26,16 @@ class Unit(models.Model):
         return self.unit_text
 
 
+@enum.unique
+class TaskState(ChoicesEnum):
+   NEW = 0
+   CONTINUING = 1
+   ENDED = 2
+
+
 class Task(models.Model):
     task_text = models.CharField(max_length=200)
-    state = models.IntegerField(default=1)
+    state = models.SmallIntegerField(choices=TaskState.choices(), blank=False, null=False)
     quantifier = models.CharField(max_length=200)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     emoji = models.ForeignKey(Emoji, on_delete=models.CASCADE)
