eca44a5778e7c35a2820201bd84dbc5ce0bc4406
Matt Williams <matt@milliams.com>
2018-03-28 10:34:37 +0100

    Make emoji field be larger

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Emoji(models.Model):
-    emoji_code = models.CharField(max_length=5)
+    emoji_code = models.CharField(max_length=50)
 
     def __str__(self):
         return self.emoji_code
