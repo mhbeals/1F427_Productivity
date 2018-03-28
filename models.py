2e6d16a8dcbebc628388a94a2c6a77c348e15e85
M. H. Beals <m.h.beals@lboro.ac.uk>
2018-03-28 09:44:11 +0100

    Corrections to ID fields

--- models.py
+++ models.py
@@ -1,25 +1,24 @@
 from django.db import models
 
 class Task(models.Model):
-    id = models.IntegerField(default=1)
     task_text = models.CharField(max_length=200)
     state = models.IntegerField(default=1)
     quantifier = models.CharField(max_length=200)
-    unit_id = models.IntegerField()
-    emoji_id = models.IntegerField()
+    unit = models.ForeignKey(Emoji, on_delete=models.CASCADE)
+    emoji = models.ForeignKey(Unit, on_delete=models.CASCADE)
     timestamp = models.DateTimeField('task completed')
     user_id = models.IntegerField()
     def __str__(self):
         return self.task_text
     
 class Emoji(models.Model):
-    emoji_id = models.ForeignKey(unit_id, on_delete=models.CASCADE)
+    emoji_id = models.ForeignKey(Task, on_delete=models.CASCADE)
     emoji_code = models.CharField(max_length=5)    
     def __str__(self):
         return self.emoji_code
 
 class Unit(models.Model):
-    unit_id = models.ForeignKey(unit_id, on_delete=models.CASCADE)
+    unit_id = models.ForeignKey(Task, on_delete=models.CASCADE)
     unit_text = models.CharField(max_length=200)
     def __str__(self):
         return self.unit_text
