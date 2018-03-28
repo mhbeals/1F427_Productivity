6eccece021c6688c19984c7fc5a4616673e6ad4b
M. H. Beals <m.h.beals@lboro.ac.uk>
2018-03-28 09:46:30 +0100

    Updated task-unit-emoji relationships

--- models.py
+++ models.py
@@ -4,21 +4,19 @@ class Task(models.Model):
     task_text = models.CharField(max_length=200)
     state = models.IntegerField(default=1)
     quantifier = models.CharField(max_length=200)
-    unit = models.ForeignKey(Emoji, on_delete=models.CASCADE)
-    emoji = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    emoji = models.ForeignKey(Emoji, on_delete=models.CASCADE)
     timestamp = models.DateTimeField('task completed')
     user_id = models.IntegerField()
     def __str__(self):
         return self.task_text
     
 class Emoji(models.Model):
-    emoji_id = models.ForeignKey(Task, on_delete=models.CASCADE)
     emoji_code = models.CharField(max_length=5)    
     def __str__(self):
         return self.emoji_code
 
 class Unit(models.Model):
-    unit_id = models.ForeignKey(Task, on_delete=models.CASCADE)
     unit_text = models.CharField(max_length=200)
     def __str__(self):
         return self.unit_text
