37ede07b0b6e10e058c463748ae2c5a086879990
James Graham <J.Graham@software.ac.uk>
2018-03-28 09:13:20 +0100

    Create initial Django project structure

---
+++ u1f427_productivity/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for u1f427_productivity project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "u1f427_productivity.settings")
+
+application = get_wsgi_application()
